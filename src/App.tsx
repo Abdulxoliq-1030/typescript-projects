2cbe4f16f4b369099727e7bdd458e0985b94a57c
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-01-14 18:19:43 +0500

    qr code

--- src/App.tsx
+++ src/App.tsx
@@ -1,140 +1,31 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import { useMemo } from "react"
-import { Container } from "react-bootstrap"
-import { Routes, Route, Navigate } from "react-router-dom"
-import { NewNote } from "./NewNote"
-import { useLocalStorage } from "./useLocalStorage"
-import { v4 as uuidV4 } from "uuid"
-import { NoteList } from "./NoteList"
-import { NoteLayout } from "./NoteLayout"
-import { Note } from "./Note"
-import { EditNote } from "./EditNote"
+import React, { useState } from "react";
+import QRCode from "react-qr-code";
 
-export type Note = {
-  id: string
-} & NoteData
+interface AppProps {}
 
-export type RawNote = {
-  id: string
-} & RawNoteData
-
-export type RawNoteData = {
-  title: string
-  markdown: string
-  tagIds: string[]
-}
-
-export type NoteData = {
-  title: string
-  markdown: string
-  tags: Tag[]
-}
-
-export type Tag = {
-  id: string
-  label: string
-}
-
-function App() {
-  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
-  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
-
-  const notesWithTags = useMemo(() => {
-    return notes.map(note => {
-      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
-    })
-  }, [notes, tags])
-
-  function onCreateNote({ tags, ...data }: NoteData) {
-    setNotes(prevNotes => {
-      return [
-        ...prevNotes,
-        { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) },
-      ]
-    })
-  }
-
-  function onUpdateNote(id: string, { tags, ...data }: NoteData) {
-    setNotes(prevNotes => {
-      return prevNotes.map(note => {
-        if (note.id === id) {
-          return { ...note, ...data, tagIds: tags.map(tag => tag.id) }
-        } else {
-          return note
-        }
-      })
-    })
-  }
-
-  function onDeleteNote(id: string) {
-    setNotes(prevNotes => {
-      return prevNotes.filter(note => note.id !== id)
-    })
-  }
-
-  function addTag(tag: Tag) {
-    setTags(prev => [...prev, tag])
-  }
-
-  function updateTag(id: string, label: string) {
-    setTags(prevTags => {
-      return prevTags.map(tag => {
-        if (tag.id === id) {
-          return { ...tag, label }
-        } else {
-          return tag
-        }
-      })
-    })
-  }
-
-  function deleteTag(id: string) {
-    setTags(prevTags => {
-      return prevTags.filter(tag => tag.id !== id)
-    })
-  }
-
+const App: React.FC<AppProps> = () => {
+  const [value, setValue] = useState("");
   return (
-    <Container className="my-4">
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <NoteList
-              notes={notesWithTags}
-              availableTags={tags}
-              onUpdateTag={updateTag}
-              onDeleteTag={deleteTag}
-            />
-          }
-        />
-        <Route
-          path="/new"
-          element={
-            <NewNote
-              onSubmit={onCreateNote}
-              onAddTag={addTag}
-              availableTags={tags}
-            />
-          }
-        />
-        <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
-          <Route index element={<Note onDelete={onDeleteNote} />} />
-          <Route
-            path="edit"
-            element={
-              <EditNote
-                onSubmit={onUpdateNote}
-                onAddTag={addTag}
-                availableTags={tags}
-              />
-            }
-          />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Container>
-  )
-}
+    <div
+      style={{
+        height: "auto",
+        margin: "0 auto",
+        width: "200px",
+      }}>
+      <QRCode
+        size={256}
+        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+        value={value}
+        viewBox={`0 0 256 256`}
+      />
+      <input
+        type="text"
+        style={{ marginTop: 30 }}
+        onChange={(e: any) => setValue(e.target.value)}
+        value={value}
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
