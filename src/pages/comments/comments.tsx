061542c5bc49a3e42c0a1ea7272bc6701c5cd8d4
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-01-15 21:42:36 +0500

    comments page complete

--- src/pages/comments/comments.tsx
+++ src/pages/comments/comments.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import cls from "./style.module.scss";
 
 interface CommentsProps {}
 
-const Comments: React.FC<CommentsProps> = () => <h1>Comments Component</h1>;
+const Comments: React.FC<CommentsProps> = () => {
+  const [data, setData] = useState([]);
+
+  async function getComments() {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/comments"
+    );
+    return setData(data);
+  }
+
+  useEffect(() => {
+    getComments();
+  }, []);
+
+  return (
+    <div className={cls.wrap}>
+      {data.map(({ id, name, body, email }) => (
+        <div key={id} className={cls.wrapper}>
+          <h1>{name}</h1>
+          <div className={cls.footer}>
+            <h3>{email}</h3>
+            <h3>{body}</h3>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Comments;
